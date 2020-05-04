2c5b7dc5113381271f95fa931f180ab5edfabbce
Grzegorz Sienkiewicz <grzegorz.sienkiewicz95@gmail.com>
2020-05-04 20:07:25 +0200

    Interacting with DOM elements

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
     templateElement: HTMLTemplateElement; // enable because added in tsconifg lib: dom
     hostElement: HTMLDivElement;
     element: HTMLFontElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
 
     constructor() {
         // równoważne znakowi ! na końcu, ktory mowi nie boj sie, ten element istnieje, zaufaj mi...
@@ -14,7 +17,23 @@ class ProjectInput {
         this.hostElement = document.getElementById('app')! as HTMLDivElement;
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild as HTMLFontElement;
+        this.element.id = "user-input";
+
+        this.titleInputElement = this.element.querySelector("#title") as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector("#description") as HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector("#people") as HTMLInputElement;
+
+        this.configure();
         this.attach();
+    }
+
+    private submitHandler(e: Event) {
+        e.preventDefault();
+        console.log(this.titleInputElement.value);
+    }
+
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this))
     }
 
     private attach() {
