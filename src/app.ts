d9d85c13b4eb5004ae7fb72d25108db808220a03
Grzegorz Sienkiewicz <grzegorz.sienkiewicz95@gmail.com>
2020-05-06 22:09:00 +0200

    Rendering Projects Lists

--- src/app.ts
+++ src/app.ts
@@ -45,12 +45,40 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
     return adjDescripton;
 }
 
+// ProjectList Class
+class ProjectList {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLElement; // HTMLSectionElement does not exist
+
+    constructor(private type: 'active' | 'finished') {
+        this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild as HTMLElement;
+        this.element.id = `${this.type}-projects`;
+        this.attach();
+        this.renderContent();
+    }
+
+    private renderContent() {
+        const listId = `${this.type}-projects-list`;
+        this.element.querySelector('ul')!.id = listId;
+        this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS';
+
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('beforeend', this.element);
+    }
+}
+
 
 // ProjectInput Class
 class ProjectInput {
     templateElement: HTMLTemplateElement; // enable because added in tsconifg lib: dom
     hostElement: HTMLDivElement;
-    element: HTMLFontElement;
+    element: HTMLFormElement;
     titleInputElement: HTMLInputElement;
     descriptionInputElement: HTMLInputElement;
     peopleInputElement: HTMLInputElement;
@@ -65,7 +93,7 @@ class ProjectInput {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement; // have to add in type
         this.hostElement = document.getElementById('app')! as HTMLDivElement;
         const importedNode = document.importNode(this.templateElement.content, true);
-        this.element = importedNode.firstElementChild as HTMLFontElement;
+        this.element = importedNode.firstElementChild as HTMLFormElement;
         this.element.id = "user-input";
 
         this.titleInputElement = this.element.querySelector("#title") as HTMLInputElement;
@@ -140,3 +168,5 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
+const activePrjList = new ProjectList('active');
+const finishedPrjList = new ProjectList('finished');
