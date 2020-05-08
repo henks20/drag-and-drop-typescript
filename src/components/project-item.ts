c352f5c752fc2a47088bd0fde15dd0e9dc0c598e
Grzegorz Sienkiewicz <grzegorz.sienkiewicz95@gmail.com>
2020-05-08 10:55:52 +0200

    organizing files & folders

---
+++ src/components/project-item.ts
@@ -0,0 +1,48 @@
+/// <reference path="base-component.ts" />
+
+namespace App {
+  // ProjectItem Class
+  export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>
+    implements Draggable {
+    private project: Project;
+
+    get persons() {
+      if (this.project.people === 1) {
+        return "1 person";
+      } else {
+        return `${this.project.people} persons`;
+      }
+    }
+
+    constructor(hostId: string, project: Project) {
+      super("single-project", hostId, false, project.id);
+      this.project = project;
+
+      this.configure();
+      this.renderContent();
+    }
+
+    @autobind
+    dragStartHandler(e: DragEvent) {
+      // console.log(e);
+      e.dataTransfer!.setData("text/plain", this.project.id);
+      e.dataTransfer!.effectAllowed = "move";
+    }
+
+    dragEndHandler(_: DragEvent) {
+      console.log("DragEnd");
+    }
+
+    configure() {
+      this.element.addEventListener("dragstart", this.dragStartHandler); // or .bind(this) instead of @autobind
+      this.element.addEventListener("dragend", this.dragEndHandler); // or .bind(this) instead of @autobind
+    }
+
+    renderContent() {
+      this.element.querySelector("h2")!.textContent = this.project.title;
+      this.element.querySelector("h3")!.textContent =
+        this.persons + " assigned";
+      this.element.querySelector("p")!.textContent = this.project.description;
+    }
+  }
+}
